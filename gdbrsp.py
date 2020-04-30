69e4f096b2c13c371b27d8a901014cf261630825
Ilya Rozentsvaig <int3rsys@live.com>
2020-04-30 13:18:43 +0300

    Update gdbrsp.py
    Fixed a couple of issues with expanding pattern + visual fixes

--- gdbrsp.py
+++ gdbrsp.py
@@ -11,13 +11,12 @@ def checksum(packet):
     return packet_sum % 256
 
 def check_integrity(pkt):
-    start = pkt[:1]
-    end = pkt[-3:-2]
-    if checksum(pkt[:-3]) != int(pkt[-2:],16):
+    if checksum(pkt[1:-3]) != int(pkt[-2:],16):
         return False
     return True
 
 def read_packet(packet):
+    packet = binascii.unhexlify(packet)
     if(packet[-3] != 35):
         return NOT_COMPLETE_SEQUENCE
     correct_integrity = check_integrity(packet)
@@ -32,39 +31,41 @@ def readfile(filepath):
     return data
 
 def decode_data(data):
-    data = data.replace('\n', '').replace('\r', '').replace('+','')
-    data = binascii.unhexlify(data)
-    data = data.split(b'$')
+    #data = data.strip()
+    #data = binascii.unhexlify(data)
+    data = data.split('\n')
     return data
 
 data = decode_data(readfile('<yourfilehere>')) #file should contain raw hex data
-partial_packet=b""
+partial_packet=""
 ignore_packet = False
 response=False
-for packet in data:
-    if partial_packet != b"":
-        # The packet contains an escape char
-        packet = partial_packet+packet
-        partial_packet=b""
-    if len(packet)>1 or ignore_packet:
-        if packet[-1] == ord(b'+'):
-            packet=packet[:-1]
-            response = False
-        if packet[-1] == ord(b'-'):
-            next_packet = data.index(packet)+1
-            ignore_packet = True
-            print("[*] Packet {} was requested to be retransmitted. Ignoring it.".format(next_packet))
-            response = False
 
-        ignore_packet = False
-        return_value = read_packet(packet)
-        if return_value == BAD_PACKET:
-            print("[*] Packet {} is corrupted".format(packet))
-        elif return_value == NOT_COMPLETE_SEQUENCE:
-            partial_packet = packet + b'$'
-        else:
-            packet = packet[:-3]
-            if not packet == b'': #there is no point in sending an empty packet...
-                parser = packet_parser.parser(packet)
-                parsed_msg = parser.get_parsed_packet()()
-                print(parsed_msg)
+with open(r'a.out','wb') as of:
+    for packet in data:
+        if partial_packet != "":
+            # The packet contains an escape char
+            packet = partial_packet+packet
+            partial_packet=""
+        if len(packet)>2 or ignore_packet:
+            if int(packet[-2:],16) == ord('+'):
+                packet=packet[:-1]
+                response = False
+            if int(packet[-2:],16) == ord('-'):
+                next_packet = data.index(packet)+1
+                ignore_packet = True
+                print("[*] Packet {} was requested to be retransmitted. Ignoring it.".format(next_packet))
+                response = False
+
+            ignore_packet = False
+            return_value = read_packet(packet)
+            if return_value == BAD_PACKET:
+                print("[*] Packet {} is corrupted".format(packet))
+            elif return_value == NOT_COMPLETE_SEQUENCE:
+                partial_packet = packet + '$'
+            else:
+                packet = packet[1*2:-3*2]
+                if not packet == b'': #there is no point in sending an empty packet...
+                    parser = packet_parser.parser(packet)
+                    parsed_msg = parser.get_parsed_packet()()
+                    of.write(parsed_msg + b'\n')
